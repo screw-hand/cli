79447c5d4ff14b55e55ed9897c2b226fa4644b9d
screw-hand <screwhand0@gmail.com>
2023-08-04 11:06:29 +0800

    perf: example/cli-generate-code.js error configList

---
+++ example/cli-generate-code.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process')
+
+const template = '<p>label:{label} -- value: {value}</p>'
+
+const configList = [
+  'label1 value1',
+  'label2 value2',
+  'label3 value3',
+  'label4 value4',
+  'label5 value5',
+]
+
+const command = `cli generate-code '${template}' '[${configList}]'`
+
+exec(command, (error, stdout, stderr) => {
+  console.log(command)
+  if (error) {
+    console.log(`error: ${error.message}`)
+    return
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`)
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+})
